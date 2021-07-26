02d638969f9f17f290e8992741efa72582851e7a
Karpagamsree <karpagamsree29@gmail.com>
2021-07-26 16:56:10 +0530

    This program is to check int
    This program is to check int primitive data type

---
+++ datatypes/intExample.java
@@ -0,0 +1,10 @@
+public class intExample {
+
+    public static void main(String[] args) {
+        int i = 3;
+        int j = 4;
+        System.out.println("print the intExample i ::" + i);
+        System.out.println("print the intExample j ::" + j);
+    }
+
+}
