af555d85584ebac74233074b2244cce18bd993df
Karpagamsree <karpagamsree29@gmail.com>
2021-07-26 17:00:07 +0530

    tried to put a big value for int
    tried to put a big value for int and got error

--- datatypes/intExample.java
+++ datatypes/intExample.java
@@ -1,8 +1,14 @@
 public class intExample {
-
     public static void main(String[] args) {
         int i = 3;
         int j = 4;
+        /* 
+		tried to put a big value for int
+        int k = 23482398409; 
+        intExample.java:6: error: integer number too large
+        int k = 23482398409; 
+
+        */
         System.out.println("print the intExample i ::" + i);
         System.out.println("print the intExample j ::" + j);
     }
